244acb1fb05be64b50cb6c84cea10473237f08ca
dahyun <boppq1@gmail.com>
2026-03-17 10:49:54 +0900

    MemDto, ResumDto upload

--- src/main/java/com/example/recruit/jdbc/member/MemberDto.java
+++ src/main/java/com/example/recruit/jdbc/member/MemberDto.java
@@ -1,5 +1,22 @@
 package com.example.recruit.jdbc.member;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberDto {
+	
+	String mid;
+	String mpw;
+	String mname;
+	Date mbirth;
+	String memail;
+	String mphone;
+	
 
 }
--- src/main/java/com/example/recruit/jdbc/resume/ResumeDto.java
+++ src/main/java/com/example/recruit/jdbc/resume/ResumeDto.java
@@ -1,5 +1,22 @@
 package com.example.recruit.jdbc.resume;
 
-public class ResumeDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResumeDto {
+	
+	int rno;
+	int jno;
+	String mid;
+	String address;
+	String univ;
+	String major;
+	String career;
+	String experi;
+	String intro;
+	
 }
