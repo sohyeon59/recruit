dbf1cebc3060c725bcefe490980b215e8021999a
psh <thgus6674@naver.com>
2026-03-17 16:13:37 +0900

    d

--- src/main/java/com/example/recruit/jdbc/resume/IResumeDao.java
+++ src/main/java/com/example/recruit/jdbc/resume/IResumeDao.java
@@ -8,14 +8,14 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface IResumeDao {
 
-	void insertResume(ResumeDto resumeDto);
-	void updateResume(ResumeDto resumeDto);
+	int insertResume(ResumeDto resumeDto);
+	int updateResume(ResumeDto resumeDto);
 	int deleteResume(@Param("rno") int rno);
 	
-	//이력서 전체 보기
+	// 이력서 전체 보기
 	List<ResumeList> getMyList(@Param("mid") String mid);
 	
-	//상세보기
+	// 상세보기
 	ResumeDto getMyResume(@Param("rno") int rno);
 	
 }
---
+++ src/main/java/com/example/recruit/service/ResumeService.java
@@ -0,0 +1,46 @@
+package com.example.recruit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.recruit.jdbc.resume.IResumeDao;
+import com.example.recruit.jdbc.resume.ResumeDto;
+import com.example.recruit.jdbc.resume.ResumeList;
+
+@Service
+public class ResumeService {
+
+	@Autowired
+	IResumeDao resumeDao;
+	
+	// 지원서 등록, 수정, 삭제 - 성공 1 실패 null
+	public int insertResume(ResumeDto dto) {
+		return resumeDao.insertResume(dto);
+	}
+	
+	public int updateResume(ResumeDto dto) {
+		return resumeDao.updateResume(dto);
+	}
+	
+	public int deleteResume(int rno) {
+		return resumeDao.deleteResume(rno);
+	}
+	
+	// 마이페이지 내이력서
+	public List<ResumeList> getMyList(String mid){
+		return resumeDao.getMyList(mid);
+	}
+	
+	// 이력서 상세보기
+	public ResumeDto getMyResume(int rno) {
+		return resumeDao.getMyResume(rno);
+	}
+	
+	
+	
+	
+	
+	
+}
