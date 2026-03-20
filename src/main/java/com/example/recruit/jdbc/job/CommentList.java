431463535004c2c8bbc0cc4cd9da6303158955dc
dahyun <boppq1@gmail.com>
2026-03-20 12:07:02 +0900

    comment 수정 2

---
+++ src/main/java/com/example/recruit/jdbc/job/CommentList.java
@@ -0,0 +1,13 @@
+package com.example.recruit.jdbc.job;
+
+import lombok.Data;
+
+@Data
+public class CommentList {
+	
+	private int cno;
+	private String mid;
+	private String content;
+	private String created_at;
+	
+}
